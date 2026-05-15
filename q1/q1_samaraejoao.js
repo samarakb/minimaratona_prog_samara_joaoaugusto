3b2b1f1a56c84b2e6e32101594413b7cc1c8b07c
samarakb <samarakoyke@gmail.com>
2026-05-15 14:48:23 -0300

    q1 js

---
+++ q1/q1_samaraejoao.js
@@ -0,0 +1,19 @@
+function executar() {
+    let vetor = [];
+
+    let n = parseFloat(document.querySelector("#numero").value);
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            vetor.push("FizzBuzz");
+        } else if (i % 3 === 0) {
+            vetor.push("Fizz");
+        } else if (i % 5 === 0) {
+            vetor.push("Buzz");
+        } else if (i % 7 === 0) {
+            vetor.push("Boom");
+        } else {
+            vetor.push(i);
+        }
+    }
+    document.querySelector("#saida").innerHTML = vetor.join(", ");
+}
